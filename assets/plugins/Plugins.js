c4c2f033fab0c2f1c5f774e2fdc7aa383ac4fb66
chendo <chendoec@live.com>
2022-04-05 00:35:28 -0500

    all changes

--- assets/plugins/Plugins.js
+++ assets/plugins/Plugins.js
@@ -6,6 +6,7 @@ class Plugins {
         this.containerSongs = config.containerSongs;
         this.currentSong = [];
         this.title = [];
+        this.id = [];
     }
 
     songs(music) {
@@ -13,35 +14,40 @@ class Plugins {
         const { title, track, id } = song;
 
         if(!(music === id)) return; 
-            this.createSource(track, title);
+            this.createSource(track, title, id);
         });
     }
     songEnd() {
 
         this.json.forEach(audio => {
-            const { title, track } = audio;
+            const { title, track, id } = audio;
             this.title.push(title);
             this.currentSong.push(track);
+            this.id.push(id);
         });
         
         let currentIndex = 0;
-        if(this.currentSong.length > (currentIndex+1)) 
+        if(this.currentSong.length > (currentIndex + 1)) 
         {
         this.containerSongs.addEventListener('ended', () => {
-            this.createSource(this.currentSong[currentIndex], this.title[currentIndex]);
+            if(!(currentIndex === 7)) {
+                this.createSource(this.currentSong[currentIndex], this.title[currentIndex], this.id[currentIndex]);
+                console.log(currentIndex)
+            }  
             return currentIndex += 1;
         });
         } else 
         {
-            this.currentSong.pause();
+        this.containerSongs.addEventListener('ended', () => this.createSource(this.currentSong[0], this.title[0], this.id[0]));
         }; 
         
     }
-    createSource(src, title) {
+    createSource(src, title, id) {
         const textTrack = document.querySelector('.description p');
         const source = document.createElement('source');
             source.src = `./assets/audio/${src}.mp3`;
             source.type = 'audio/mpeg';
+            source.id = id;
             textTrack.textContent = title;
             this.clearHTML();
             this.containerSongs.appendChild(source);
